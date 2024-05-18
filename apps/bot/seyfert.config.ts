9521ccdd656dfc992f78a49090445dd8ac32dafe
simxnet <simxnet@envs.net>
2024-05-18 23:17:51 +0200

    fix: type config

--- apps/bot/seyfert.config.ts
+++ apps/bot/seyfert.config.ts
@@ -1,10 +1,12 @@
+import { InternalRuntimeConfig } from "seyfert/lib/client/base";
+
 export default {
-	token: process.env.BOT_TOKEN,
-	intents: ["Guilds", "MessageContent", "GuildMessages"],
+	token: process.env.BOT_TOKEN as string,
+	intents: 513,
 	locations: {
 		base: "src",
 		output: "dist",
 		commands: "commands",
 		events: "events",
 	},
-};
+} satisfies InternalRuntimeConfig;
