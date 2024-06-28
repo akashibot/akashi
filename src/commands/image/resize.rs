916b85189092a0af2728924a91de48080ba3044b
simxnet <simxnet@envs.net>
2024-06-28 14:07:11 +0200

    feat: use width if height is invalid

--- src/commands/image/resize.rs
+++ src/commands/image/resize.rs
@@ -7,15 +7,13 @@ use crate::{
 
 fn parse_size(size_str: &str) -> Result<(u32, u32), String> {
     let parts: Vec<&str> = size_str.split('x').collect();
-    if parts.len() != 2 {
-        return Err("Invalid format".into());
-    }
-    let width = parts[0]
+    let width = parts.get(0)
+        .ok_or_else(|| "Invalid format".to_owned())?
         .parse::<u32>()
         .map_err(|_| "Invalid width".to_owned())?;
-    let height = parts[1]
-        .parse::<u32>()
-        .map_err(|_| "Invalid height".to_owned())?;
+    let height = parts.get(1)
+        .map_or(Ok(width), |h| h.parse::<u32>().map_err(|_| "Invalid height".to_owned()))
+        .unwrap_or(width);
 
     Ok((width, height))
 }
@@ -31,6 +29,7 @@ pub enum ResizeTypeChoices {
 /// Resize an image
 ///
 /// `resize 200x200`
+/// `resize 200` ("height" will be the same as "width now")
 #[poise::command(
     prefix_command,
     track_edits,
@@ -40,8 +39,8 @@ pub enum ResizeTypeChoices {
 )]
 pub async fn resize(
     ctx: Context<'_>,
-    #[description = "New image size (E.g: 200x200)"] size: String,
-    #[description = "Resize type (default: Enlarge)"] #[rename = "type"] resize_type: Option<ResizeTypeChoices>,
+    #[description = "New image size (E.g: 200x200 or 200)"] size: String,
+    #[description = "Resize type (default: enlarge)"] #[rename = "type"] resize_type: Option<ResizeTypeChoices>,
     #[description = "Image url"] url: Option<String>,
     #[description = "Image attachment"] attachment: Option<serenity_prelude::Attachment>,
 ) -> Result<(), Error> {
@@ -54,11 +53,19 @@ pub async fn resize(
     Ok(())
 }
 
-#[test]
-fn test_parse_size() {
-    assert_eq!(parse_size("200x200").unwrap(), (200, 200));
-    assert_eq!(parse_size("100x100").unwrap(), (100, 100));
-    assert!(parse_size("200").is_err());
-    assert!(parse_size("200x").is_err());
-    assert!(parse_size("x200").is_err());
+#[cfg(test)]
+mod resize_tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_size() {
+        assert_eq!(parse_size("200x200").unwrap(), (200, 200));
+        assert_eq!(parse_size("100x100").unwrap(), (100, 100));
+        assert_eq!(parse_size("200").unwrap(), (200, 200));
+        assert!(parse_size("200").is_ok());
+        assert!(parse_size("200x").is_ok());
+        assert_eq!(parse_size("200x").unwrap(), (200, 200));
+        assert!(parse_size("x200").is_err());
+        assert!(parse_size("").is_err());
+    }
 }
