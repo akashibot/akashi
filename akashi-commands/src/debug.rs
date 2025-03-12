868590186e33c425fb664f90506df4ec975809a6
simxnet <simxnet@envs.net>
2025-03-12 15:58:53 +0100

    fix: use normal register builtin

--- akashi-commands/src/debug.rs
+++ akashi-commands/src/debug.rs
@@ -1,14 +1,8 @@
 use akashi_shared::{AkashiData, AkashiError};
 use poise::Command;
 
-mod disable_command;
-mod enable_command;
 mod register;
 
 pub fn register() -> Vec<Command<AkashiData, AkashiError>> {
-	vec![
-		register::register(),
-		disable_command::dev_disable_command(),
-		enable_command::dev_enable_command(),
-	]
+	vec![register::register()]
 }
