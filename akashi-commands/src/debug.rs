81d46c9bbac6854d190746d00aa4007f51d9b034
simxnet <simxnet@envs.net>
2025-03-13 22:28:59 +0100

    feat: add servers command

--- akashi-commands/src/debug.rs
+++ akashi-commands/src/debug.rs
@@ -2,7 +2,8 @@ use akashi_shared::{AkashiData, AkashiError};
 use poise::Command;
 
 mod register;
+mod servers;
 
 pub fn register() -> Vec<Command<AkashiData, AkashiError>> {
-	vec![register::register()]
+	vec![register::register(), servers::servers()]
 }
---
+++ akashi-commands/src/debug/servers.rs
@@ -0,0 +1,8 @@
+use akashi_shared::{AkashiContext, AkashiResult};
+
+#[poise::command(prefix_command, owners_only, hide_in_help)]
+pub async fn servers(ctx: AkashiContext<'_>) -> AkashiResult {
+    poise::builtins::servers(ctx).await?;
+
+    Ok(())
+}
