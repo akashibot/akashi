7139a362c9c859a14899f9ba8f0489c70e4b0844
simxnet <simxnet@envs.net>
2025-03-16 00:01:09 +0100

    fix: inconsistent lines

--- akashi-commands/src/util/vote.rs
+++ akashi-commands/src/util/vote.rs
@@ -5,9 +5,9 @@ use akashi_shared::{AkashiContext, AkashiResult};
 pub async fn vote(ctx: AkashiContext<'_>) -> AkashiResult {
 	ctx.reply(
 		"> You can vote for Akashi in the following botlists: (direct links)
-        \n[top.gg](https://top.gg/bot/1288610561956773979/vote)
-        \n[dlist.gg](https://discordlist.gg/bot/1288610561956773979/vote?action=vote)
-        \n[dsc.bot](https://nightly.dsc.bot/akashi.4672/vote)",
+        [top.gg](<https://top.gg/bot/1288610561956773979/vote>)
+        [dlist.gg](<https://discordlist.gg/bot/1288610561956773979/vote?action=vote>)
+        [dsc.bot](<https://nightly.dsc.bot/akashi.4672/vote>)",
 	)
 	.await?;
 
